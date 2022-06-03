bca8f18acde0a169a348e31478a96660c0278b59
King-Del-ErnestO <delernestoa@gmail.com>
2022-06-03 12:24:22 +0100

    initial commit

--- app.py
+++ app.py
@@ -64,5 +64,7 @@ def submit():
         return render_template('index.html', message='You have already submitted feedback!')
 
 
-if __name__ == "__main__":
-    app.run()
+
+port = int(os.environ.get('PORT', 5000))
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=port)
